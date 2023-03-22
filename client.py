d497f49ad1a7b2c22cfcc341513461070d9a70b1
neha26pp <panditn856@gmail.com>
2023-03-22 00:26:01 -0400

    change color of final score based on value

--- client.py
+++ client.py
@@ -300,17 +300,21 @@ async def compare_pipeline(_):
     for i in external.learning_outcomes:
         ext_chunk += i
 
-    summary = (await get_summary(psu_chunk, ext_chunk, (round(comparer.final_score * 100, 2)))).to_py()
+    # summary = (await get_summary(psu_chunk, ext_chunk, (round(comparer.final_score * 100, 2)))).to_py()
 
     fig = graph(psu.learning_outcomes, comparer.averages)
     PyScript.write('graph', fig)
 
+    if(comparer.final_score > 0.5):
+        document.getElementById("final-score").style.color = "green"
+    else:
+        document.getElementById("final-score").style.color = "red"
     # writing final score to screen
     PyScript.write(
         "final-score", str(round(comparer.final_score * 100, 2)) + "%", append=False)
     # chatgpt summary to screen
-    PyScript.write(
-        "summary", summary['choices'][0]['message']['content'], append=False)
+    # PyScript.write(
+    #     "summary", summary['choices'][0]['message']['content'], append=False)
 
     '''Attributes of the comparer can also be easily accessed due to the
     Comparer class. For usage examples, see:
